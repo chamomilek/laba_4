795a0abf2dd513d5f2ca4fa2c547659681996475
Karina Osoprilko <chamomileekk@gmail.com>
2020-12-03 05:16:30 +0300

    start working

---
+++ src/bsu/rfe/java/group10/lab4/Osoprilko/varB/GraphicsDisplay.java
@@ -0,0 +1,75 @@
+package bsu.rfe.java.group10.lab4.Osoprilko.varB;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDisplay extends JPanel {
+    // Список координат точек для построения графика
+    private Double[][] graphicsData;
+    private Double [][] graphicsData2;
+    // Флаговые переменные, задающие правила отображения графика
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    // Границы диапазона пространства, подлежащего отображению
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    // Используемый масштаб отображения
+    private double scale;
+    // Различные стили черчения линий
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    // Шрифт отображения подписей к осям координат
+    private Font axisFont;
+
+    public GraphicsDisplay() {
+// Цвет заднего фона области отображения - белый
+        setBackground(Color.WHITE);
+// Сконструировать необходимые объекты, используемые в рисовании
+// Перо для рисования графика
+        graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_ROUND, 10.0f, null, 0.0f);
+// Перо для рисования осей координат
+        axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+// Перо для рисования контуров маркеров
+        markerStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+// Шрифт для подписей осей координат
+        axisFont = new Font("Serif", Font.BOLD, 36);
+    }
+
+//--------------------------------------------------------------------------------------
+
+    // Метод вызывается из обработчика элемента меню "Открыть файл с графиком"
+// главного окна приложения в случае успешной загрузки данных
+    public void showGraphics(Double[][] graphicsData) {
+// Сохранить массив точек во внутреннем поле класса
+        this.graphicsData = graphicsData;
+// Запросить перерисовку компонента (неявно вызвать paintComponent())
+        repaint();
+    }
+    public void showGraphics2(Double[][] graphicsData2)
+    {
+        // Сохранить массив точек во внутреннем поле класса
+        this.graphicsData2 = graphicsData2;
+        // Запросить перерисовку компонента, т.е. неявно вызвать paintComponent()
+        repaint();
+    }
+
+//---------------------------------------------------------------------------------------
+// Методы-модификаторы для изменения параметров отображения графика
+// Изменение любого параметра приводит к перерисовке области
+    public void setShowAxis(boolean showAxis) {
+        this.showAxis = showAxis;
+        repaint();
+    }
+    public void setShowMarkers(boolean showMarkers) {
+        this.showMarkers = showMarkers;
+        repaint();
+    }
+
+}
---
+++ src/bsu/rfe/java/group10/lab4/Osoprilko/varB/MainFrame.java
@@ -0,0 +1,4 @@
+package bsu.rfe.java.group10.lab4.Osoprilko.varB;
+
+public class MainFrame {
+}
